010cc985ccf472b54847da7e3a3434e89449c80f
kbowen99 <kurtisbowen99@gmail.com>
2015-12-15 09:34:42 -0600

    Actually Working

--- Hailstone/src/recursion/Hail.java
+++ Hailstone/src/recursion/Hail.java
@@ -1,5 +1,55 @@
 package recursion;
+import java.math.BigInteger;
 
 public class Hail {
-
+	
+	BigInteger iterations = new  BigInteger("0");
+	BigInteger maxReached = new BigInteger("0");
+	
+	//Basic code is basic
+	@Deprecated
+	public int blackMagic(int n){
+		System.out.print(n + ", ");
+		if (n == 1){
+			return n;
+		} else if (n%2 == 0) {
+			return blackMagic(n/2);
+		} else {
+			return blackMagic(3*n+1);
+		}
+	}
+	
+	public BigInteger bigBlackMagic(BigInteger n){
+		System.out.print(n + ", ");
+		iterations = iterations.add(BigInteger.ONE);
+		
+		if (n.compareTo(maxReached) > 0){
+			maxReached = n;
+		}
+		
+		if (n.equals(BigInteger.ONE)){
+			return n;
+		} else if (n.mod(new BigInteger("2")).equals(BigInteger.ZERO)) {
+			return bigBlackMagic(n.divide(new BigInteger("2")));
+		} else {
+			return bigBlackMagic((n.multiply(new BigInteger("3"))).add(new BigInteger("1")));
+		}
+	}
+	
+	public BigInteger bigBlackMagic(String n){
+		return bigBlackMagic(new BigInteger(n));
+	}
+	
+	@Deprecated
+	public BigInteger bigBlackMagic(int n){
+		return bigBlackMagic(new BigInteger(Integer.toString(n)));
+	}
+	
+	public BigInteger getIterations(){
+		return iterations;
+	}
+	
+	public BigInteger getMaxReached(){
+		return maxReached;
+	}
 }
--- Hailstone/src/recursion/Main.java
+++ Hailstone/src/recursion/Main.java
@@ -1,10 +1,22 @@
 package recursion;
+import java.math.BigInteger;
+
+import javax.swing.JOptionPane;
 
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+		try {
+			Hail hell = new Hail();
+			String BigNum = JOptionPane.showInputDialog("Where we startin?");
+			hell.bigBlackMagic(new BigInteger(BigNum));
+			String outputS = "";
+			outputS += "Stats: "+ "\n";
+			outputS += " - Iterations: " + hell.getIterations().toString() + "\n";
+			outputS += " - Max Reached: " + hell.getMaxReached().toString() + "\n";
+			JOptionPane.showMessageDialog(null, outputS);
+		} catch (Exception e){}
 	}
 
 }
